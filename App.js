aedb3ce0dc08222b85b76a3cfd0ea918ebe6086e
James Oluwaleye <oluwaleyejames@gmail.com>
2024-11-07 11:08:53 +0100

    ...

--- App.js
+++ App.js
@@ -1,28 +1,87 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [words, setWords] = useState("");
+  const [myGoals, setMyGoals] = useState([]);
+
+  const handleGoalInput = (enteredText) => {
+    setWords(enteredText);
+  };
+  const handleAddGoal = () => {
+    setMyGoals((currentMyGoals) => [...currentMyGoals, words]);
+    setWords("");
+  };
+
+  const handleRemove = (index) => {
+    setMyGoals((currentMyGoals) =>
+      currentMyGoals.filter((_, goalIndex) => goalIndex !== index)
+    );
+  };
   return (
     <View style={styles.container}>
-     <View style={{flexDirection: 'row', justifyContent:'space-between', gap: 3, }}>
-      <TextInput placeholder="Enter your goal: "/>
-      <Button title="Add "/>
-     </View>
-     <View>
-      <Text>Goals Loading...</Text>
-     </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-around",
+          gap: 3,
+          alignItems: "center",
+          marginBottom: 24,
+          borderBottomWidth: 1,
+          borderBottomColor: "#ccc",
+          flex: 1,
+        }}
+      >
+        <TextInput
+          style={styles.textInput}
+          value={words}
+          placeholder="Enter your goal: "
+          onChangeText={handleGoalInput}
+        />
+        <Button title="Add Goal " onPress={handleAddGoal} />
+      </View>
+      <View style={{ flex: 5 }}>
+        <ScrollView>
+          {myGoals.map((goal, index) => (
+            <View
+              key={index}
+              style={{
+                padding: 12,
+                backgroundColor: "blue",
+                marginBottom: 8,
+                borderRadius: 12,
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={{ paddingVertical: 2, color: "white" }}>{goal}</Text>
+              <Text onPress={() => handleRemove(index)}>Remove</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 50,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flex: 1,
   },
-  dummyText: {
-    alignItems: "flex-start",
-    flex: 0,
-    fontSize: 16,
-    fontFamily: 'apple',
-    color: 'green'
-  }
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    padding: 8,
+    marginRight: 8,
+  },
 });
